fa7c260632e99d4b610b2c34e8273f6a67f0ff91
Brett <brettdansmith@gmail.com>
2025-10-01 16:16:15 -0400

    Complete basic linked list. Add package.json

--- datastructures/linkedList.js
+++ datastructures/linkedList.js
@@ -1,18 +1,123 @@
-class LinkedList {
+class Node {
+    constructor(val=null, next=null) {
+        this.val    = val;
+        this.next   = next;
+    }
+}
+
+export class LinkedList {
     
-    constructor(data, next) {
-        if(data == undefined) {
-            throw new Error('data parameter is required');
-        }
-        this.data=data;
-        if(next !== undefined){
-            this.next=next;
-        }
+    constructor() {
+        this.head = null;
+        this.tail = null;
     }
-
+    
     append(value) {
-        
+        const node = new Node(value);
+        if(!this.head){
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
     }
 
+    prepend(value) {
+        const node = new Node(value);
+        if(!this.head) {
+            this.head = this.tail = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
 
+    size() {
+        let count = 0;
+        let current = this.head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+
+    getHead() {
+        return this.head.val;
+    }
+
+    getTail() {
+        return this.tail.val;
+    }
+
+    at(index) {
+        if( index < 0 ) throw new Error('Index cannot be less than 0');
+        let curr = this.head.val;
+        let i = 0;
+        
+        while( curr != null ) {
+            if ( i === index ) return curr;
+            curr = curr.next;
+            i++;
+        }
+
+        return null;
+    }
+
+    pop() {
+
+        if( !this.head ) return null;
+
+        if( this.head === this.tail ) {
+            const removed = this.head;
+            this.head = this.tail = null;
+            return removed.val;
+        }
+
+        let prev = this.head;
+
+        while( prev.next !== this.tail ) {
+            prev = prev.next;
+        }
+
+        const removed = this.tail;
+        this.tail = prev;
+        this.tail.next = null;
+        return removed.val;
+    }
+
+    contains(value) {
+    let current = this.head;
+
+        while( current !== null ) {
+            if( current.val === value ) return true;
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    find(value) {
+        let current = this.head;
+        let index = 0;
+        while( current !== null ) {
+            if( current.val === value ) return index;
+
+            index++;
+            current = current.next;
+        }
+
+        return -1;
+    }
+
+    toString() {
+        let current = this.head;
+
+        while( current != null ) {
+            console.log("(  " + current.val + "  )  ->  ");
+            current = current.next;
+        }
+        console.log("null");
+    }
 }
---
+++ index.js
@@ -0,0 +1,13 @@
+import { LinkedList } from './datastructures/linkedList.js';
+
+const ll = new LinkedList();
+
+ll.append("dog");
+ll.append("cat");
+ll.append("parrot");
+ll.append("hamster");
+ll.append("snake");
+ll.append("turtle");
+
+ll.toString();
+
